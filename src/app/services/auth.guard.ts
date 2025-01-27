83f7ae732237a312fc1401cdebe4e15144dff14e
Jack Margeson <margeson.jack@gmail.com>
2025-01-26 22:13:05 -0500

    Add two new routing guards for admin and cataloger roles

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,6 +2,7 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
@@ -9,6 +10,48 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   if (authService.isAuthenticated()) {
     return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+};
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return authService.isRole('admin').pipe(
+      map((isRole: boolean) => {
+        if (isRole) {
+          return true;
+        } else {
+          router.navigate(['/dashboard']);
+          return false;
+        }
+      })
+    );
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+};
+
+export const catalogerGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return authService.isRole('cataloger').pipe(
+      map((isRole: boolean) => {
+        if (isRole) {
+          return true;
+        } else {
+          router.navigate(['/dashboard']);
+          return false;
+        }
+      })
+    );
   } else {
     router.navigate(['/login']);
     return false;
