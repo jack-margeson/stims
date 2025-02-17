a25fe359d375909542951aa4a0e92640ce03b929
Jack Margeson <margeson.jack@gmail.com>
2025-02-17 13:43:26 -0500

    Syntax fix

--- sql/create_admin_user.sql
+++ sql/create_admin_user.sql
@@ -1,7 +1,7 @@
 -- CREATE ADMIN USER
 
 INSERT INTO users (username, first_name, last_name, email, password_hash) VALUES 
-    {$1, $2, $3, $4, crypt($5, gen_salt('bf'))};
+    ($1, $2, $3, $4, crypt($5, gen_salt('bf')));
 
 INSERT INTO user_roles (user_id, role_id) VALUES 
     ((SELECT user_id FROM users WHERE username = $1), 
