b6f0035fa7708d1ff59a286249d9606f6ab8e534
Jack Margeson <margeson.jack@gmail.com>
2025-02-17 13:37:55 -0500

    Add admin role assignment during user creation

--- sql/create_admin_user.sql
+++ sql/create_admin_user.sql
@@ -2,3 +2,7 @@
 
 INSERT INTO users (username, first_name, last_name, email, password_hash) VALUES 
     {$1, $2, $3, $4, crypt($5, gen_salt('bf'))};
+
+INSERT INTO user_roles (user_id, role_id) VALUES 
+    ((SELECT user_id FROM users WHERE username = $1), 
+    (SELECT role_id FROM roles WHERE role_name = 'admin'));
